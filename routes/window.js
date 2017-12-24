19558e92aa3da107c8bac1fee7226467f455a90c
Hiranandani, Himanshu (US - Bengaluru) <hhiranandani@DELOITTE.com>
2017-12-24 11:25:48 +0530

    updating get and post to the same function to simplify debugging

--- routes/window.js
+++ routes/window.js
@@ -2,19 +2,22 @@ var express = require('express');
 var router = express.Router();
 var windowModel = require('../models/window');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('Found 4 windows connected');
-});
-
-router.post('/', function(req, res, next) {
+const manageWindows = () => {
   windowModel(req.body).then((data) => {
     res.send(data);
   }).catch((err) => {
     console.log('ERROR OCCURRED WHILE MANAGING WINDOWS');
     res.send(err);
   })
+}
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  manageWindows(req, res);
 });
 
+router.post('/', function(req, res, next) {
+  manageWindows(req, res);
+});
 
 module.exports = router;
