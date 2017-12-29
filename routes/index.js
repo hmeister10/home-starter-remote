30e5120bb53f78c93fda6e2af66210a2eba4aa8f
Hiranandani, Himanshu (US - Bengaluru) <hhiranandani@DELOITTE.com>
2017-12-29 16:50:11 +0530

    creates an index post route

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/', function(req, res, next) {
+  res.send(req.body);
+});
+
 
 module.exports = router;
